68b9bdc6ef2e15c5e03e6da5cb3056332b0268a5
BVer <dr0joon@gmail.com>
2022-02-23 04:19:00 +0900

    feature : backned make api

--- Backend/mnist/models.py
+++ Backend/mnist/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+import uuid
+import os
+
+
+# 파일 이름 랜덤
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('', filename)
 
 
 # Create your models here.
 class ImageInformation(models.Model):
     name = models.CharField(max_length=100)
-    images = models.ImageField(name="number")
+    images = models.ImageField(name="number", upload_to=get_file_path)
 
     def str(self):
         return self.name
-
--- Backend/mnist/views.py
+++ Backend/mnist/views.py
@@ -2,10 +2,12 @@ from http.client import HTTPResponse
 
 import numpy as np
 import tensorflow as tf
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from mnist.models import ImageInformation
 from django.views.decorators.csrf import csrf_exempt
+from werkzeug.utils import secure_filename  # 파일 안정성 검사
 
 
 # Create your views here.
@@ -37,9 +39,11 @@ def image_prediction(request):
 
 @method_decorator(csrf_exempt)
 def save_predict(request):
+    global file_index
     if request.method == "POST":
-        result = []
+        result = ""
         image_file = request.FILES['image_file']
+        # image_file.name = secure_filename(image_file.name)
         ImageInformation.objects.create(
             name=request.POST['name'],
             number=image_file
@@ -49,8 +53,9 @@ def save_predict(request):
             if image.number == image_file:
                 result = predict(image.number)
                 print(image.number)
+                print(image_file.name)
                 break
-        return render(request, 'test.html', {"image": result})
+        return JsonResponse(result, status=200)
 
 
 def predict(image):
@@ -71,4 +76,5 @@ def predict(image):
             index = i
     # print(index)
     # print(scores[0])
-    return [index, best_score]
+    result = {'index': index, 'percent': float(best_score)}
+    return result
