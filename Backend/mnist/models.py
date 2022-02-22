1e9a1ee858f30f95f92582b8665c56b188723b60
BVer <dr0joon@gmail.com>
2022-02-22 21:36:08 +0900

    feature django setting

--- Backend/mnist/models.py
+++ Backend/mnist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class ImageInformation(models.Model):
+    name = models.CharField(max_length=100)
+    images = models.ImageField(name="number", width_field=28, height_field=28)
+
+    def str(self):
+        return self.name
+
--- Backend/mnist/views.py
+++ Backend/mnist/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from mnist.models import ImageInformation
 
 # Create your views here.
+
+
+def image(request):
+    if request.method == "POST":
+        ImageInformation.objects.create(
+            name=request.POST['name']
+        )
+
+def image_prediction(request):
+    if request.method == "POST":
+        image_file = request.POST['image_file']
+        images = ImageInformation.objects.all().order_by("-id")
+        for image in images:
+            if image.images == image_file:
+                return
+                # mnist search
+
+
