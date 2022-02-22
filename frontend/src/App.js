2abceafaa4f25253e12d48dbe1e5bd3dc506f225
JoeCP17 <at365@naver.com>
2022-02-23 02:32:11 +0900

    React 화면구성

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ImageUpload from './ViewComponent/ImageUpload'
+import './ViewComponent/style.scss'
+function App() {
+  return (
+      <ImageUpload></ImageUpload>
+  );
+}
+
+export default App;
