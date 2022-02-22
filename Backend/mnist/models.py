29ab4cb27fa5c19d99e7b07a1574cef288fde763
BVer <dr0joon@gmail.com>
2022-02-22 23:36:08 +0900

    feature : backned change files

--- Backend/mnist/models.py
+++ Backend/mnist/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class ImageInformation(models.Model):
     name = models.CharField(max_length=100)
-    images = models.ImageField(name="number", width_field=28, height_field=28)
+    images = models.ImageField(name="number")
 
     def str(self):
         return self.name
--- Backend/mnist/views.py
+++ Backend/mnist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from mnist.models import ImageInformation
 
+
 # Create your views here.
 
 
@@ -10,13 +11,23 @@ def image(request):
             name=request.POST['name']
         )
 
+
 def image_prediction(request):
+    global image
     if request.method == "POST":
         image_file = request.POST['image_file']
         images = ImageInformation.objects.all().order_by("-id")
         for image in images:
             if image.images == image_file:
-                return
+                print(image)
                 # mnist search
-
+        return render(request, 'templates/test.html', {"image": image})
+    else:
+        image_file = request.POST['image_file']
+        images = ImageInformation.objects.all().order_by("-id")
+        for image in images:
+            if image.images == image_file:
+                print(image)
+                # mnist search
+        return render(request, 'templates/test.html', {"image": image})
 
